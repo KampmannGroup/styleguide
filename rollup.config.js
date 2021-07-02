3998bd71a43df833107759297949519efcbd24e4
Matthias Fänger <matthias.faenger@muuuh.de>
2021-07-02 14:25:58 +0200

    Add rollup to create npm package bundles for js and css

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import { terser } from 'rollup-plugin-terser'
+import scss from 'rollup-plugin-scss'
+import pkg from './package.json'
+import copy from 'rollup-plugin-copy'
+
+export default {
+  input: 'src/assets/js/index.js',
+  plugins: [
+    terser(),
+    scss({
+      output: 'dist/kampmann.min.css',
+      outputStyle: 'compressed',
+    }),
+    copy({
+      targets: [{ src: ['src/assets/fonts/**/*.woff', 'src/assets/fonts/**/*.woff2'], dest: 'dist/fonts' }],
+    }),
+  ],
+  output: [
+    {
+      name: 'kampmann',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+}
