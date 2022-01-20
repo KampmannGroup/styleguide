19c3424081bf72f7c4d45b33b07c7cc70b15231c
Matthias Fänger <matthias.faenger@muuuh.de>
2022-01-20 12:02:51 +0100

    Run build

--- src/compiled/assets/js/card.js
+++ src/compiled/assets/js/card.js
@@ -1,7 +1,7 @@
 import Swiper, { Navigation, Pagination, Autoplay, Controller } from 'swiper'
 Swiper.use([Pagination, Autoplay, Navigation, Controller])
 
-import Tab from 'bootstrap'
+import { Tab } from 'bootstrap'
 
 document.addEventListener('DOMContentLoaded', function (event) {
   //Images
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-      disabledClass: 'disabled'
+      disabledClass: 'disabled',
     },
   })
 
   //Tabs
   var triggerTabList = [].slice.call(document.querySelectorAll('.card__tab'))
   triggerTabList.forEach(function (triggerEl) {
-    var tabTrigger = new bootstrap.Tab(triggerEl)
+    var tabTrigger = new Tab(triggerEl)
 
     triggerEl.addEventListener('click', function (event) {
       event.preventDefault()
