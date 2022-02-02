50689a8dfa7f46009bcfd0de951de49be820f10b
Savas Tireng <savas.tireng@muuuh.de>
2022-02-02 15:29:21 +0100

    Base set up

---
+++ _docs/components/data-table.md
@@ -0,0 +1,7 @@
+---
+title: Data Tables
+info:
+nav: false
+---
+
+{% include pattern_block.html url='/src/patterns/components/data-table/primary-table.html' %}
---
+++ src/assets/js/data-table.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const table = new DataTable("#table_id", {
+    paging: false,
+    searching: false,
+    info: false,
+  });
+});
