578614ad708a82b965e416fa114af97ba6c23751
Katrin Zimmermann <katrin.zimmermann@muuuh.de>
2022-06-13 15:17:27 +0200

    Test
    # Conflicts:
    #	dist/css/kampmann.min.css
    #	dist/js/kampmann.esm.min.js
    #	dist/js/kampmann.umd.min.js
    #	src/assets/js/header.js
    #	src/compiled/assets/css/style.css
    #	src/compiled/assets/css/style.css.map
    #	src/compiled/assets/js/header.js
    #	src/compiled/patterns/templates/header/header_onpage.html
    #	src/patterns/templates/header/header_onpage.html
    
    # Conflicts:
    #	dist/css/kampmann.min.css
    #	dist/js/kampmann.esm.min.js
    #	dist/js/kampmann.umd.min.js
    #	src/assets/js/header.js
    #	src/compiled/assets/css/style.css
    #	src/compiled/assets/css/style.css.map
    #	src/compiled/assets/js/header.js
    #	src/compiled/patterns/templates/header/header_onpage.html
    #	src/patterns/templates/header/header_onpage.html

--- src/compiled/assets/js/header.js
+++ src/compiled/assets/js/header.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   const header = document.querySelector('header.header')
   const headerMeta = header.querySelector('.header__section--meta')
+  const meta = header.querySelector('.header__section--meta')
+  const metaHeight = meta.offsetHeight
+  const main = header.querySelector('.header__section--main')
+  const mainHeight = main.offsetHeight
+  const thirdNav = document.querySelector('.header-third-nav-dummy')
   const languageSwitch = header.querySelector('#language-switch')
 
   let oldValue = 0
@@ -30,13 +35,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
     const meta = header.querySelector('.header__section--meta')
     if (pos === 0 || languageSwitch.classList.contains('show') || dir === 'Up') {
       meta.style.marginTop = ''
+      if(thirdNav){
+        thirdNav.style.top = `${(mainHeight + metaHeight - 1)}px`
+      }
       return
     }
 
     if (dir === 'Down' && pos > 0) {
-      meta.style.marginTop = `-${headerMeta.offsetHeight}px`
+      meta.style.marginTop = `-${(metaHeight + 1)}px`
+      if(thirdNav){
+        thirdNav.style.top = `${(mainHeight - 2)}px`
+      }
       return
     }
+  }
+  if(thirdNav){
+    thirdNav.style.top = `${mainHeight - 2}px`
   }
 
   window.addEventListener('scroll', (e) => checkScrollDirectionAndPosition(e))
